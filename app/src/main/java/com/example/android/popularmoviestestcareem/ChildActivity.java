19950bc66fbeb1945f2ad452f2c9f28f921a207e
Marylinemds <mendes.escom@gmail.com>
2018-03-29 18:14:42 +0200

    I added the toolbar in childActivity

--- app/src/main/java/com/example/android/popularmoviestestcareem/ChildActivity.java
+++ app/src/main/java/com/example/android/popularmoviestestcareem/ChildActivity.java
@@ -8,9 +8,11 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.ImageView;
@@ -43,11 +45,8 @@ import java.util.regex.Pattern;
  */
 
 
-public class ChildActivity extends AppCompatActivity implements VideoAdapter.ListItemClickHandler{
+public class ChildActivity extends AppCompatActivity{
 
-
-    RecyclerView mVideosList;
-    VideoAdapter videoAdapter;
 
 
     ImageView movieDisplay;
@@ -63,10 +62,6 @@ public class ChildActivity extends AppCompatActivity implements VideoAdapter.Lis
 
     String voteAverage;
 
-    List<Video> videos = new ArrayList<>();
-
-
-
 
     public String id;
 
@@ -77,10 +72,21 @@ public class ChildActivity extends AppCompatActivity implements VideoAdapter.Lis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        if (toolbar != null){
+            setSupportActionBar(toolbar);
+        }
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white);
+        getSupportActionBar().setTitle("CareemTest");
+        toolbar.setTitleTextColor(ContextCompat.getColor(this,R.color.white));
+
         movieDisplay = (ImageView) findViewById(R.id.display_movie);
         originalTitle_tv = (TextView) findViewById(R.id.movie_title);
         originalTitle_tv_2 = (TextView) findViewById(R.id.movie_title_2);
-        releaseDate_tv= (TextView) findViewById(R.id.release_date);
+        releaseDate_tv = (TextView) findViewById(R.id.release_date);
         voteAverage_tv = (TextView) findViewById(R.id.vote_average_2);
         voteAverage_rb = (RatingBar) findViewById(R.id.vote_average);
         sypnosis_tv = (TextView) findViewById(R.id.sypnosis);
@@ -94,14 +100,14 @@ public class ChildActivity extends AppCompatActivity implements VideoAdapter.Lis
 
                 String moviePath = movie.getMoviePath();
                 id = movie.getId();
-                System.out.println("blabla " + id );
+                System.out.println("blabla " + id);
 
 
                 originalTitle_tv.setText(movie.getOriginalTitle());
                 originalTitle_tv_2.setText(movie.getOriginalTitle());
-                releaseDate_tv.setText(movie.getReleaseDate().substring(0,4));
+                releaseDate_tv.setText(movie.getReleaseDate().substring(0, 4));
                 voteAverage = movie.getUserRating();
-                voteAverage_rb.setRating((Float.valueOf(voteAverage))/2);
+                voteAverage_rb.setRating((Float.valueOf(voteAverage)) / 2);
                 voteAverage_tv.setText("(" + movie.getUserRating() + "/10)");
                 sypnosis_tv.setText(movie.getSynopsis());
 
@@ -110,92 +116,11 @@ public class ChildActivity extends AppCompatActivity implements VideoAdapter.Lis
             }
         }
 
-
-        mVideosList = (RecyclerView) findViewById(R.id.rv_videos);
-
-        GridLayoutManager layoutManager;
-
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            layoutManager = new GridLayoutManager(this, 3);
-        } else {
-            layoutManager = new GridLayoutManager(this, 4);
-        }
-
-        mVideosList.setLayoutManager(layoutManager);
-
-        videoAdapter = new VideoAdapter(this);
-
-
-        mVideosList.setAdapter(videoAdapter);
-
-        makeTheQueryVideos();
-
-
-    }
-
-    public void makeTheQueryVideos(){
-
-        URL SearchUrl = NetworkUtils.buildUrlVideo(id);
-        String searchUrl = SearchUrl.toString();
-        RequestQueue queue = Volley.newRequestQueue(this);
-
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, searchUrl,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String JSONData) {
-                        if (JSONData != null) {
-
-                            try {
-
-                                JSONObject objJSON = new JSONObject(JSONData);
-                                JSONArray results = objJSON.getJSONArray("results");
-                                Video video;
-
-                                for (int i = 0; i < results.length(); i++) {
-
-                                    JSONObject resultsData = results.getJSONObject(i);
-
-                                    String key = resultsData.getString("key");
-                                    String videoName = resultsData.getString("name");
-
-                                    video = new Video();
-                                    video.setKey(key);
-                                    video.setVideoName(videoName);
-                                    videos.add(video);
-
-                                }
-
-                                videoAdapter.setVideos(videos);
-                                videoAdapter.notifyDataSetChanged();
-
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
     }
 
 
 
-    @Override
-    public void onClick(String mVideoId) {
-        // open video Youtube
 
-        Matcher matcher = Pattern.compile("http://www.youtube.com/embed/").matcher(mVideoId);
-        matcher.find();
-        Intent VideoIntent = new Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse("https://www.youtube.com/watch?v=" + mVideoId));
-        startActivity(VideoIntent);
-    }
 
 
 }
